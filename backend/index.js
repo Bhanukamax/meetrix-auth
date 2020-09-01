8393906152a95049893ff449f39005ee1226cea2
Bhanuka <bhanukalm@gmail.com>
2020-09-01 13:23:56 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 4000;
+
+app.post("/auth", async (req, res) => {
+  if (!req.header("Authorization")) return res.send(400);
+
+  const auth = req.header("Authorization");
+
+  const parsed = await jwt.decode(auth);
+  if (!parsed) return res.send(400);
+
+  return res.send(parsed);
+});
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
